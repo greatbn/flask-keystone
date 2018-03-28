10e4899479d02c4835afe7d78530de9a2db74367
sapd <sapd@vccloud.vn>
2018-03-28 15:50:56 +0700

    Init commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from keystonemiddleware import auth_token
+
+app = Flask(__name__)
+
+KEYSTONE_AUTHTOKEN = {
+    'auth_url': 'http://10.5.9.58:5000/v3',
+    'auth_plugin': 'password',
+    'username': 'admin',
+    'password': 'vccloud',
+    'project_name': 'admin',
+    'user_domain_name': 'default',
+    'project_domain_name': 'default'
+}
+
+app.wsgi_app = app.wsgi_app = auth_token.AuthProtocol(app.wsgi_app, KEYSTONE_AUTHTOKEN)
+
+
+@app.route("/")
+def index():
+    """
+    Test authen
+    """
+    return "Authenticated"  
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
